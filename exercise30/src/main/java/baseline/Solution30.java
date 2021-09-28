aacb6caff994298a0321e8aecb4f1282933377e4
Perry-codes <89555259+Perry-codes@users.noreply.github.com>
2021-09-28 18:53:08 -0400

    ex30 pseudo

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Maggie Perry
+ */
+
+package baseline;
+
+public class Solution30 {
+
+    /*
+    Create a program that generates a multiplication table for the numbers 1 through 12 (inclusive).
+
+    Constraint
+• Use a nested loop to complete this program.
+• Align each column within the table without using the tab character.
+     */
+    public static void main(String[] args) {
+
+        //just call multiplication table
+        multiplicationTable();
+
+
+    }
+
+    public static void multiplicationTable() {
+        //read in max value for table
+        //find out the max space needed for largest value+2
+        //loop 2 layer loop for multiplication and print out result
+        //use integer formatting to space results
+    }
+}
