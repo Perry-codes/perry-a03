afefafd1ba9608985ce3b48c8585ac5a82d6ab9f
Perry-codes <89555259+Perry-codes@users.noreply.github.com>
2021-10-02 19:29:12 -0400

    ex28 code commit

--- exercise28/src/main/java/baseline/Solution28.java
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -5,7 +5,14 @@
 
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution28 {
+
+    //solution class for testing
+    public void Solution28(){
+
+    }
 
     /*
     Write a program that prompts the user for five numbers and computes the total of the numbers.
@@ -14,20 +21,38 @@ public class Solution28 {
 • The prompting must use repetition, such as a counted loop, not three separate prompts.
      */
     //Scanner for user input
-    //Scanner sc = new
-    //use array to store 5 numbers
-    double[] fiveNumberArray = new double[5];
+    public static final Scanner sc = new Scanner(System.in);
+
 
     public static void main(String[] args) {
+        //use array to store 5 numbers
+        double[] array = new double[5];
         //create loop
         //while less than 5 prompt user for number
         //save input into array
+        int i =0;
+        while (i < 5){
+            //prompt for input
+            System.out.print("Enter a number:");
+            if(sc.hasNextDouble()){
+                array[i] = sc.nextDouble();
+                i++;
+            } else sc.next();
+        }
         //send array to add function
-        addNumbers(double[] fiveNumberArray);
+        //new solution class to use non static methods
+        Solution28 s = new Solution28();
+        //addNumbers(fiveNumberArray);
+        System.out.printf("The total is %.2f.",s.addNumbers(array));
 
     }
 
-    public static void addNumbers(double[] fiveNumberArray) {
+    public double addNumbers(double[] fiveNumberArray) {
         //loop to add array values
+        int sum = 0;
+        for (double v : fiveNumberArray) {
+            sum += v;
+        }
+        return sum;
     }
 }
--- exercise28/src/test/java/baseline/Solution28Test.java
+++ exercise28/src/test/java/baseline/Solution28Test.java
@@ -2,13 +2,19 @@ package baseline;
 
 import org.junit.jupiter.api.Test;
 
+import javax.management.ConstructorParameters;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution28Test {
 
+
+
     @Test
     void addNumbers() {
         //push 5 numbers to addNumbers function return sum
-
+        Solution28 s = new Solution28();
+        double[] testArray = {1, 2, 3, 4, 5};
+        assertEquals(15, s.addNumbers(testArray) );
     }
 }
