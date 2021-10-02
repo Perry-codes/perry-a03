fd719e6933b33b850238315d25354158352cf469
Perry-codes <89555259+Perry-codes@users.noreply.github.com>
2021-10-02 18:35:24 -0400

    ex27 code commit

--- exercise27/src/main/java/baseline/Solution27.java
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -5,8 +5,13 @@
 
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution27 {
-   /*
+    //Scaner for user input
+    private static final Scanner sc = new Scanner(System.in);
+
+    /*
    Write a program that prompts for a first name, last name, employee ID, and ZIP code. Ensure that the input is
 valid according to these rules:
 • The first name must be filled in.
@@ -24,54 +29,92 @@ data and invokes the specific validation functions.
 
     public static void main(String[] args) {
         //prompt first name
-        String firstName = nextLine;
+        System.out.print("Enter the first name:");
+        String firstName = sc.nextLine();
         //prompt last name
-        lastName = nextIn;
+        System.out.print("Enter the last name:");
+        String lastName = sc.nextLine();
         //prompt zip code
-        zipCode = nextIn;
+        System.out.print("Enter the ZIP code:");
+        String zipCode = sc.nextLine();
         //prompt employee id
-        empID = nextIn;
+        System.out.print("Enter the employee ID:");
+        String empID = sc.nextLine();
 
         /*Create a function for each validation rule. Then create a validateInput
         function that takes in all of the input data and invokes the specific
         validation functions.*/
-        validateInput();
+        Solution27 s = new Solution27();
+        s.validateInput(firstName,lastName,zipCode,empID);
 
     }
 
-    public static void validateInput(String firstName,String lastName, String zipCode,
+    private void validateInput(String firstName,String lastName, String zipCode,
                                      String empID) {
+        //use string builder to build output string
         //call validation for rules
-        validateFirstName(firstName);
-        validateLastName(lastName);
-        validateZipCode(zipCode);
-        validateEmpID(empID);
+        StringBuilder outputString = new StringBuilder(validateFirstName(firstName));
+        outputString = outputString.append(validateLastName(lastName));
+        outputString = outputString.append(validateZipCode(zipCode));
+        outputString = outputString.append(validateEmpID(empID));
 
         //use a single statement for outputs
-        output();
+        String singleString = (outputString.isEmpty() ? "No errors were found." : outputString.toString());
+        //output();
+        System.out.printf("%s",singleString);
     }
 
-    public static String validateEmpID(String empID) {
-        //is string null
+    //function to determine if there are any errors
+    private boolean noErrors(){
+
+        return true;
+    }
+
+    public String validateEmpID(String empID) {
+        String idError = "The employee ID must be in the format of AA-1234.\n";
+        //is string null or wrong length
+        if(empID.length() != 7) return idError;
         //check string format
-        return null;
+        //string to char array
+        char[] temp = empID.toCharArray();
+        if(!Character.isLetter(temp[0])) return idError;
+        if(!Character.isLetter(temp[1])) return idError;
+        if(temp[2] != 45) return idError;
+        for (int i = 3; i < 7; i++) {
+            if (!Character.isDigit(temp[i])) return idError;
+        }
+        return "";
     }
 
-    public static String validateZipCode(String zipCode) {
-        //is string null
-        //is string al numbers
-        return "return string";
+    public String validateZipCode(String zipCode) {
+        String zipError = "We only care about 'MERICA, so zipcode must be 5 digit number\n";
+        //is string null or wrong length
+        if(zipCode.length() != 5) return zipError;
+        //is string all numbers
+        try {
+            Integer.parseInt(zipCode);
+            return "";
+        } catch(NumberFormatException e){
+            return zipError;
+        }
+
     }
 
-    public static String validateLastName(String lastName) {
+    public String validateLastName(String lastName) {
         //is string null
+        if(lastName == "") return "The last name must be filled in.\n";
         //does string have min 2 char
-        return "return string";
+        if(lastName.length()<3) return "The last name must be at least 2 characters long.";
+
+        return "";
     }
 
-    public static String validateFirstName(String firstName) {
+    public String validateFirstName(String firstName) {
         //is string null
+        if(firstName == "") return "The first name must be filled in.\n";
         //does string have min 2 char
-        return "Return string";
+        if(firstName.length()<3) return "The first name must be at least 2 characters long.\n";
+
+        return "";
     }
 }
--- exercise27/src/test/java/baseline/Solution27Test.java
+++ exercise27/src/test/java/baseline/Solution27Test.java
@@ -6,28 +6,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Solution27Test {
 
-    @Test
-    void validateInput() {
-        //also uses all subs
-    }
 
     @Test
     void validateEmpID() {
         //input emp ID return string
+        Solution27 s = new Solution27();
+
+        assertEquals("",s.validateEmpID("AA-1234"));
     }
 
     @Test
     void validateZipCode() {
         //input zip code return string
+        Solution27 s = new Solution27();
+        String zipError = "We only care about 'MERICA, so zipcode must be 5 digit number\n";
+        assertEquals(zipError,s.validateZipCode("L56 5Q4"));
     }
 
     @Test
     void validateLastName() {
         //input string return string
+        Solution27 s = new Solution27();
+        String lNameNullError = "The last name must be filled in.\n";
+        assertEquals(lNameNullError,s.validateLastName(""));
     }
 
     @Test
     void validateFirstName() {
         //input string return string
+        Solution27 s = new Solution27();
+        String fNameNullError = "The last name must be at least 2 characters long.\n";
+        assertEquals(fNameNullError,s.validateFirstName("M"));
     }
 }
