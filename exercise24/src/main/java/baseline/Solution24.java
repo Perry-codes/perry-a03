67f4b1e736ed04d3f73ddb22eadc7c528ae37af0
Perry-codes <89555259+Perry-codes@users.noreply.github.com>
2021-10-02 11:32:39 -0400

    ex24 code commit

--- exercise24/src/main/java/baseline/Solution24.java
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -4,6 +4,10 @@
  */
 
 package baseline;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 /*
 Exercise 24 - Anagram Checker
 Using functions to abstract the logic away from the rest of your code makes it easier to read and easier to
@@ -21,25 +25,59 @@ returns true or false.
 
  */
 public class Solution24 {
+
+    //String variable
+    private final String inputString;
+    //Create scanner
+    private static final Scanner sc =  new Scanner(System.in);
+
+    //class constructor
+    public Solution24(String inputString){
+        this.inputString = inputString;
+    }
+
+    public String toString(){
+        return this.inputString;
+    }
+
     public static void main(String[] args) {
 
         //prompt user for string1
+        System.out.println("Anagram Checker\nEnter first string:");
+        //make string object 1
+        String input1 = sc.nextLine();
+        Solution24 s1 = new Solution24(input1);
         //prompt user for string2
-        //remove spaces and sort strings
-        sortString(String string1);
-        sortString();
+        System.out.println("Enter second string:");
+        //make string object 2
+        String input2 = sc.nextLine();
+        Solution24 s2 = new Solution24(input2);
         //check with isAnagram
-        isAnagram();
+        Boolean anagram = s1.isAnagram(s1.inputString, s2.inputString);
+
+        if(anagram){
+            System.out.printf("\"%s\" and \"%s\" are anagrams",s1.inputString,s2.inputString);
+        }else{
+            System.out.printf("\"%s\" and \"%s\" are not anagrams",s1.inputString,s2.inputString);
+        }
 
     }
 
-    public static Boolean isAnagram() {
+    public Boolean isAnagram(String inputString1, String inputString2) {
+        //check if sorted
+        return sortString(inputString1).equals(sortString(inputString2));
 
     }
 
-    public static String sortString(String string1, String string2) {
+    public String sortString(String inputString) {
         //remove spaces
+        String noSpace = inputString.replace(" ","");
         //set all letters to lowercase
-        //sort remaining
+        String allLower = noSpace.toLowerCase();
+        //sort remaining with temp character array and sort function
+        char[] tempArray = allLower.toCharArray();
+        Arrays.sort(tempArray);
+
+        return new String(tempArray);
     }
 }
--- exercise24/src/test/java/baseline/Solution24Test.java
+++ exercise24/src/test/java/baseline/Solution24Test.java
@@ -2,15 +2,38 @@ package baseline;
 
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution24Test {
 
     @Test
-    void isAnagram() {
+    void testIsAnagram() {
+        Solution24 s1 = new Solution24("Test");
+        Solution24 s2 = new Solution24("TTeS");
+
+        assertTrue(s1.isAnagram(s1.toString(),s2.toString()));
     }
 
     @Test
-    void sortString() {
+    void testSortString() {
+        Solution24 s = new Solution24("Test");
+        String test = s.sortString(s.toString());
+        assertEquals("estt", test);
+    }
+
+    @Test
+    void testSortStringWithSpaces() {
+        Solution24 s = new Solution24("Test One Two");
+        String test = s.sortString(s.toString());
+        assertEquals("eenoostttw", test);
+    }
+
+    @Test
+    void testIsAnagramWitSpaces() {
+        Solution24 s1 = new Solution24("The Morse Code");
+        Solution24 s2 = new Solution24("Here come dots");
+
+        assertTrue(s1.isAnagram(s1.toString(),s2.toString()));
     }
 }
