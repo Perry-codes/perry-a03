d1829d1be829fade84cbf7c047f8297d48e585fd
Perry-codes <89555259+Perry-codes@users.noreply.github.com>
2021-10-03 19:26:50 -0400

    ex36 code commit

--- exercise36/src/main/java/baseline/Solution36.java
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -5,17 +5,27 @@
 
 package baseline;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Solution36 {
 
+    private static final Scanner sc = new Scanner(System.in);
+
     //make class constructor with array input for testing
-    public Solution36(int[] numberArray){
+    public Solution36(ArrayList<Integer> numberArray){
         this.numberArray = numberArray;
+    }
+    //make empty constructor or main run use
+    public Solution36(){
+
     }
 
 
     /*
     Write a program that prompts for response times from a website in milliseconds. It should keep asking for
-values until the user enters “done.”
+values until the user enters done.
 The program should print the average time (mean), the minimum time, the maximum time, and the population
 standard deviation.
 
@@ -27,58 +37,90 @@ Constraints
 • Keep the input separate from the processing and the output.
      */
 
-    //integer array to store inputs
-    private int[] numberArray = new int[];
-    //double variables for avg and std deviation
-    private double arrayAvg;
-    private double stdDeviation;
-    //interger variables for min and max
-    private int arrayMin;
-    private int arrayMax;
-
-    //input numbers to array
-    public void inputArray() {
-        //prompt user for number input
-        //check if done, exit
-        //if not number ignore and wait for number input
-        //
-    }
-
-    //average function
-    public void average() {
-        //loop to count number of inputs and add inputs
-        int counter;
-        int sum;
-        this.arrayAvg = sum / counter;
-    }
-
-    //max function
-    public void max() {
-        //set int max = 0
-        int max =0;
-        //loop through array, if bigger than max, set max equal
-        this.arrayMax = max;
-    }
-
-    //min function
-    public void min() {
-        //set int min = to first array value numberArray[0]
-        int min;
-        //loop through array, if less than max, set max equal
-        this.arrayMax = min;
-    }
-
-    //std deviation function
-    public void std() {
-        //std deviation = sqrt ( sum ( i - array.average ) ^ 2 / array.length )
-
-
-    }
-
-
+    //integer arrayList to store inputs
+    private ArrayList<Integer> numberArray = new ArrayList<>();
 
 
     public static void main(String[] args) {
+        //create class to run non-static methods
+        Solution36 s = new Solution36();
+        s.inputArray();
+        System.out.println(s);
 
     }
+
+    //input numbers to array
+    public void inputArray() {
+        while(true){
+            //prompt user for number input
+            System.out.print("Enter a number:");
+            String input = sc.nextLine();
+            //check if done, exit
+            if (input.equals("done")) break;
+            try{//parse input to int - input should not be float type, unrealistic number precision
+                int numberInput = Integer.parseInt(input);
+                numberArray.add(numberInput);
+            }//if not number ignore and wait for number input
+            catch (NumberFormatException e){
+                System.out.println("Integer number input only\n");
+            }
+        }
+    }
+
+    //average function
+    public double average(List<Integer> avgArray) {
+        //loop to count number of inputs and add inputs
+        int sum = 0;
+
+        for (Integer o : avgArray) {
+            sum += o;
+        }
+        if (avgArray.isEmpty()) return 0;
+
+        return (double)sum / avgArray.size();
+    }
+
+    //max function
+    public int max(List<Integer> maxArray) {
+        //set int max = 0
+        int max = maxArray.get(0);
+        //loop through array, if bigger than max, set max equal
+        for (int i=1; i < maxArray.size(); i++){
+            max = (maxArray.get(i) > max ? maxArray.get(i) : max);
+        }
+        return max;
+    }
+
+    //min function
+    public int min(List<Integer> minArray) {
+        //set int min = to first array value numberArray[0]
+        int min =minArray.get(0);
+        //loop through array, if less than max, set max equal
+        for (int i=1; i < minArray.size(); i++){
+            min = (minArray.get(i) < min ? minArray.get(i) : min);
+        }
+        return min;
+    }
+
+    //std deviation function
+    public double std(List<Integer> stdArray) {
+        //std deviation = sqrt ( sum ( i - array.average ) ^ 2 / array.length )
+        //find avg
+        double avg = average(stdArray);
+        //sum of (number - avg)^2
+        int sum = 0;
+        for (Integer integer : stdArray) {
+            sum += Math.pow((integer - avg), 2);
+        }
+        return Math.pow((double) sum / stdArray.size(),0.5);
+
+    }
+
+    public String toString(){
+        return String.format("Numbers: %s%nThe average is %.2f%nThe minimum is %d%nThe maximum is %d%nThe standard deviation is %.2f",
+                numberArray.toString(),average(numberArray),min(numberArray),max(numberArray),std(numberArray));
+    }
+
+
+
 }
--- exercise36/src/test/java/baseline/Solution36Test.java
+++ exercise36/src/test/java/baseline/Solution36Test.java
@@ -2,6 +2,9 @@ package baseline;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution36Test {
@@ -10,21 +13,38 @@ class Solution36Test {
 
     @Test
     void average() {
-        //check avg finction
+        //initialize a test object
+        ArrayList<Integer> testArray = new ArrayList<>(Arrays.asList(10,12,22,34,21));
+        Solution36 testObject = new Solution36(testArray);
+        //check avg function
+        assertEquals(19.8, testObject.average(testArray));
     }
 
     @Test
     void max() {
+        //initialize a test object
+        ArrayList<Integer> testArray = new ArrayList<>(Arrays.asList(10,12,22,34,21));
+        Solution36 testObject = new Solution36(testArray);
         //check max function
+        assertEquals(34, testObject.max(testArray));
     }
 
     @Test
     void min() {
+        //initialize a test object
+        ArrayList<Integer> testArray = new ArrayList<>(Arrays.asList(10,12,22,34,21));
+        Solution36 testObject = new Solution36(testArray);
         //check min function
+        assertEquals(10, testObject.min(testArray));
     }
 
     @Test
     void std() {
+        //initialize a test object
+        ArrayList<Integer> testArray = new ArrayList<>(Arrays.asList(10,12,22,34,21));
+        Solution36 testObject = new Solution36(testArray);
         //check std deviation
+        assertEquals(8.508, testObject.std(testArray),0.001);
     }
+
 }
