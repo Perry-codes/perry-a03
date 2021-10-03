e58c9512ac6c9a00d4ecc105aa960d5c45139524
Perry-codes <89555259+Perry-codes@users.noreply.github.com>
2021-10-02 21:11:35 -0400

    ex 29 code commit

--- exercise29/src/main/java/baseline/Solution29.java
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -5,7 +5,16 @@
 
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution29 {
+    //Scanner for input
+    private static final Scanner sc = new Scanner(System.in);
+
+    //solution object for testing non static methods
+    public void Solution29(){
+
+    }
     /*
     Write a quick calculator that prompts for the rate of return on an investment and calculates how many years it
 will take to double your investment.
@@ -19,23 +28,41 @@ Constraints
 • Use a loop to trap bad input, so you can ensure that the user enters valid values.
      */
     public static void main(String[] args) {
+        Solution29 s = new Solution29();
         //use input function to take and validate input
         //r =
-        userRateofReturn();
+        double rateOfReturn = s.userRateofReturn();
 
         //send r to formula
-        yearsToDouble();
+        int years = s.yearsToDouble(rateOfReturn);
+
+        //output
+        System.out.printf("It will take %d years to double your initial investment",years);
 
     }
 
-    public static double userRateofReturn() {
+    public double userRateofReturn() {
         //prompt user for rate of return
-        //check that the input is valid
+        while (true) {
+            System.out.print("What is the rate of return?");
+            //check that the input is valid
+            if(sc.hasNextDouble()) {
+                double input = sc.nextDouble();
+                if (input != 0) return input;
+                else System.out.println("Invalid input.");
+            }else{
+                System.out.println("Invalid input.");
+                sc.nextLine();
+            }
+        }
+
         //loop for new input if not, return rate if valid
     }
 
-    public static void yearsToDouble() {
+    public int yearsToDouble(double rateOfReturn) {
         //years = 72 / r
-        //output "It will take {year} years to double your initial investment"
+        int years = (int) Math.ceil(72 / rateOfReturn);
+        return years;
+
     }
 }
--- exercise29/src/test/java/baseline/Solution29Test.java
+++ exercise29/src/test/java/baseline/Solution29Test.java
@@ -8,11 +8,13 @@ class Solution29Test {
 
     @Test
     void userRateofReturn() {
-        //NO IDEA
+        //no test for user input verification
     }
 
     @Test
     void yearsToDouble() {
         //push rate, check return
+        Solution29 s = new Solution29();
+        assertEquals(18,s.yearsToDouble(4));
     }
 }
